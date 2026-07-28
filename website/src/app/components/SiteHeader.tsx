86580a02312a2b1e76dca13da963a97b61656c70
Tanvi Mishra <tanvimishra1216@gmail.com>
2026-07-28 17:25:52 -0400

    established constants

--- website/src/app/components/SiteHeader.tsx
+++ website/src/app/components/SiteHeader.tsx
@@ -2,3 +2,11 @@
 
 import { useState } from "react";
 
+const navLinks = [
+  { name: "Learn", href: "/learn" },
+  { name: "Quick Reference", href: "/quick-reference" },
+  { name: "Project Ideas", href: "/project-ideas" },
+  { name: "Gallery", href: "/gallery" },
+  { name: "FAQs", href: "/FAQs" }
+]; 
+
